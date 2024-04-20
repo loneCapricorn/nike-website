84ecf47e8f62e2e06060a3d3ba97ad0d4fa81629
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-20 17:16:46 +0300

    Add PopularProduct section

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { star } from "../assets/icons";
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full">
+      <img src={imgURL} alt={name} width={280} height={280} />
+      <div className="mt-8 flex flex-col justify-start items-center gap-2.5">
+        <div className="flex gap-3">
+          <img src={star} alt="rating" width={24} height={24} />
+          <p className="font-montserrat text-xl leading-normal text-slate-gray">
+            (4.5)
+          </p>
+        </div>
+        <h3 className="mt-2 text-xl leading-normal font-semibold font-palanquin">
+          {name}
+        </h3>
+        <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
+          {price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+PopularProductCard.propTypes = {
+  imgURL: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+};
+
+export default PopularProductCard;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import Nav from "./Nav";
+import PopularProductCard from "./PopularproductCard";
 import ShoeCard from "./ShoeCard";
 
-export { Button, Nav, ShoeCard };
+export { Button, Nav, PopularProductCard, ShoeCard };
