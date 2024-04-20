00f68b0daaa933dde373a9f963751e6cd69484a6
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-20 19:23:33 +0300

    Add specialOffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,21 @@
 import PropTypes from "prop-types";
 
-const Button = ({ label, iconURL = null }) => {
+const Button = ({
+  label,
+  iconURL = null,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : "bg-coral-red text-white border-coral-red"
+      }
+    `}
+    >
       {label}
       {iconURL && (
         <img
