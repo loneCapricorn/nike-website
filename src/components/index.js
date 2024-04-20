0642478677ca3e4baa821712fe9ba25eba07e933
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-20 18:50:24 +0300

    Add Services section

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Nav from "./Nav";
 import PopularProductsCard from "./PopularProductsCard";
 import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
 
-export { Button, Nav, PopularProductsCard, ShoeCard };
+export { Button, Nav, PopularProductsCard, ShoeCard, ServiceCard };
